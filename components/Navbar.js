6b9f4c95a3dd80e43b349863287eb2ba6ce337d8
Charlesoluwafemi <76776186+Charlesoluwafemi@users.noreply.github.com>
2024-09-19 18:22:53 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,9 +7,9 @@ export default function Navbar() {
 
   return (
     <nav className="bg-gray-800 text-white">
-      <div className="container mx-auto p-4 flex items-center">
+      <div className="container mx-auto p-4 flex items-center justify-between">
         {/* Logo - Aligned to the Left */}
-        <div className="flex items-center justify-start">
+        <div className="flex items-center">
           <Link href="/">
             <span className="text-3xl font-extrabold cursor-pointer text-pink-600">
               BonRegal
@@ -17,7 +17,7 @@ export default function Navbar() {
           </Link>
         </div>
 
-        {/* Navigation Links - Centered */}
+        {/* Navigation Links - Centered on Desktop */}
         <div className="hidden md:flex space-x-6 mx-auto">
           <Link href="/menu-of-the-week">
             <span className="text-lg font-medium hover:text-gray-400 cursor-pointer">Menu of the Week</span>
@@ -45,8 +45,8 @@ export default function Navbar() {
           </Link>
         </div>
 
-        {/* Mobile Menu Button */}
-        <div className="md:hidden">
+        {/* Mobile Menu Button - Aligned to the Right */}
+        <div className="md:hidden ml-auto">
           <button onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? (
               <XIcon className="w-6 h-6 text-white" />
@@ -56,7 +56,7 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-      
+
       {/* Mobile Menu */}
       <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
         <div className="bg-gray-700 text-white space-y-2 p-4">
@@ -89,4 +89,5 @@ export default function Navbar() {
     </nav>
   );
 }
+
 
